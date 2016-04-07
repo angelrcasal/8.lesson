c1eeaf6e75a6a484e78f1e3bfb1b0dd08e5e6791
angcas <angcas@greeny.cs.tlu.ee>
2016-04-07 13:46:23 +0300

    correction of the last modification

--- restrict.php
+++ restrict.php
@@ -5,27 +5,20 @@
     //RESTRICTION - LOGGED IN
     if(isset($_SESSION["user_id"])){
         //redirect not logged in user to login page
-        header("Location: login.php);
+        header("Location: login.php");
         
         
     }
     
-    //?logout is on the URL
-
-    if(isset($_GET["logout"])){
-    //delete the session 
-    session_destroy();
-    
-    header("location: login.php");    
-    
-    
-}
-
+    //?logout is in the URL
+	if(isset($_GET["logout"])){
+		//delete the session
+		session_destroy();
+		
+		header("Location: login.php");
+	}
 ?>
 
-<h2> Welcome <?=$_SESSION["usename"];?> (<?=
-$_SESSION["user_id"];?>) </h2>
+<h2> Welcome <?php echo $_SESSION["username"];?> (<?=$_SESSION["user_id"];?>) </h2>
 
-<a href="?logout=1" > log out</a>
-
-restricted
+<a href="?logout=1" >Log out</a>
